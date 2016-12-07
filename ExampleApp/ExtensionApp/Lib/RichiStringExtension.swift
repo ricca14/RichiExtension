95bc4f9edcf11fbacdd399e4c799a8107829215a
Ricaldone Gianluca <g.ricaldone@trenkwalderitalia.it>
2016-12-07 10:55:59 +0100

    Struttura cartelle libreria

---
+++ ExampleApp/ExtensionApp/Lib/RichiStringExtension.swift
@@ -0,0 +1,15 @@
+//
+//  RichiStringExtension.swift
+//  ExtensionApp
+//
+//  Created by Gianluca Ricaldone on 07/12/16.
+//  Copyright © 2016 Ricca. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    
+    
+}
